68037525a45541e16417f2dc703af799510d947f
Mayank Kumar <mayankjustdial>
2026-08-07 23:57:27 +0530

    Add context to prevent zombies Queries

--- internal/handlers/listings.go
+++ internal/handlers/listings.go
@@ -29,8 +29,10 @@ func NewListingHandler(db *sql.DB) *ListingHandler {
 }
 
 func (lh ListingHandler) List(w http.ResponseWriter, r *http.Request) {
+	// request scoped context
+	ctx := r.Context()
 
-	rows, err := lh.db.Query(
+	rows, err := lh.db.QueryContext(ctx,
 		`SELECT id, title, description, price, city, create_at
 		FROM listings
 		ORDER BY create_at DESC
@@ -71,10 +73,12 @@ func (lh ListingHandler) List(w http.ResponseWriter, r *http.Request) {
 
 func (lh ListingHandler) Delete(w http.ResponseWriter, r *http.Request) {
 	// Delete
+	ctx := r.Context()
+
 	id := r.PathValue("id")
 	fmt.Println("id", id)
 
-	_, err := lh.db.Exec(
+	_, err := lh.db.ExecContext(ctx,
 		`DELETE FROM listings WHERE id = $1`, id)
 	if err != nil {
 		log.Printf("delete: %v", err)
